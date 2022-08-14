af4c0b72245e7c0fa166879626494f4dd5ae19c7
JV-Albuquerque-S <alb.albqrq@gmail.com>
2022-08-14 11:13:37 -0300

    fix: initial signIn config

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 import signUpSchema from "../schemas/signUpSchema.js";
-//import signInSchema from "../schemas/signInSchema.js";
+import signInSchema from "../schemas/signInSchema.js";
 
 import { createUser } from "../controllers/userController.js";
 //import { singIn } from "../controllers/authController.js";
@@ -11,6 +11,6 @@ import { validateSchema } from "../middlewares/schemaValidator.js";
 const authRouter = Router();
 
 authRouter.post("/signup", validateSchema(signUpSchema), createUser);
-//authRouter.post("/signin", validateSchema(signInSchema), signIn);
+authRouter.post("/signin", validateSchema(signInSchema), /*signIn*/);
 
 export default authRouter;
