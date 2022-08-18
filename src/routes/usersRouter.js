cda26ef3a01b0292c4bd2a8df3bba72036956c47
ViniciusTerra <viniterra33@gmail.com>
2022-08-17 23:44:02 -0300

    alterSearch

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,12 +1,10 @@
 import { Router } from "express";
 
-import searchSchema from "../schemas/searchSchema.js";
-import { validateSchema } from "../middlewares/schemaValidator.js";
 import { validateToken } from "../middlewares/authValidator.js";
 import { searchUsers } from "../controllers/userController.js";
 
 const usersRouter = Router();
 
-usersRouter.get('/serachUsers', validateToken, validateSchema(searchSchema), searchUsers);
+usersRouter.get('/serachUsers/:letters', validateToken, searchUsers);
 
 export default usersRouter;
